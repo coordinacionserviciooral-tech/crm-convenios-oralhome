e418fa49eceb4d6e868f940407b0eaea44fe1871
coordinacionserviciooral-tech <coordinacionserviciooral@gmail.com>
2026-05-11 07:49:59 -0500

    Update tailwind.config.js

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+   "./app/**/*.{js,ts,jsx,tsx}",
+  "./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",
+  "./src/**/*.{js,ts,jsx,tsx}", // Verifica si tus archivos están en /src
+],
   theme: {
     extend: {},
   },
